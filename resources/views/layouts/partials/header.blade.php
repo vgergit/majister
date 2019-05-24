76ad45c095f7601f6d037505c77b9e8e49898b9c
vger <vger@devenv.dev>
2019-05-24 18:26:06 +0100

    Preparing for pain...

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -4,13 +4,13 @@
 
     <h1 class="jumbotron-heading">Welcome to Majister</h1>
 
-    <p class="lead text-muted">Please log in or sign up below.</p>
+    <p class="lead text-muted">Please log in or register below.</p>
 
     <p>
 
       <a href="#" class="btn btn-primary">Log In</a>
 
-      <a href="#" class="btn btn-secondary">Sign Up</a>
+      <a href="#" class="btn btn-secondary">Register</a>
 
     </p>
 
